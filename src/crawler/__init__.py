ee112cc0067812607a0f7acaa89e7a46dc068e7d
oneulddu <84663820+oneulddu@users.noreply.github.com>
2026-06-09 22:02:02 +0900

    feat: 아카라이브 Scrapling 크롤러 추가

--- src/crawler/__init__.py
+++ src/crawler/__init__.py
@@ -1,4 +1,4 @@
-from .arcalive import ArcaLiveCrawler
+from .arcalive import ArcaLiveCrawler, ArcaLiveCrawlerV2
 from .base_crawler import ArticleCollection, BaseArticle, BaseCrawler
 from .clien import ClienCrawler
 from .coolenjoy import CoolenjoyCrawler, CoolenjoyRSSCrawler
@@ -24,6 +24,7 @@ __all__ = [
     "DamoangCrawler",
     "RuliwebCrawler",
     "ArcaLiveCrawler",
+    "ArcaLiveCrawlerV2",
     "ZodCrawler",
     "QuasarzoneCrawler",
     "QuasarzoneMobileCrawler",
